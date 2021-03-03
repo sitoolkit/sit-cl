5df2fafcb27be8b920d10c0ac397bd1926c11001
tnk-ry <79884322+tnk-ry@users.noreply.github.com>
2021-03-03 19:44:14 +0900

    Feature/add data type time and postgres json (#1)

--- sit-csvloader-core/src/main/java/io/sitoolkit/csv/core/CsvLoader.java
+++ sit-csvloader-core/src/main/java/io/sitoolkit/csv/core/CsvLoader.java
@@ -10,10 +10,12 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
 import java.sql.Timestamp;
 import java.sql.Types;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -66,7 +68,12 @@ public class CsvLoader {
     try (ResultSet rs = connection.getMetaData().getColumns(null, connection.getSchema(), tableName, "%")) {
 
       while (rs.next()) {
-        metaData.addDataType(rs.getString("COLUMN_NAME"), rs.getInt("DATA_TYPE"));
+        metaData.addDataType(
+          rs.getString("COLUMN_NAME"), 
+          metaData.new TypeDetail(
+            rs.getInt("DATA_TYPE"),
+            rs.getString("TYPE_NAME"))
+        );
       }
     }
 
@@ -135,6 +142,17 @@ public class CsvLoader {
             case Types.TIMESTAMP:
               pstmt.setTimestamp(columnIndex, Timestamp.valueOf(LocalDateTime.parse(cellValue)));
               break;
+            case Types.TIME:
+              pstmt.setTime(columnIndex, Time.valueOf(LocalTime.parse(cellValue)));
+              break;
+            case Types.OTHER:
+              if (isPgJsonColumn(connection.getMetaData().getDatabaseProductName(),
+                  metaData.getTypeName(columnName))) {
+                pstmt.setObject(columnIndex, cellValue, Types.OTHER);
+              } else {
+                pstmt.setString(columnIndex, cellValue);
+              }
+              break;
             default:
               pstmt.setString(columnIndex, cellValue);
           }
@@ -145,4 +163,10 @@ public class CsvLoader {
       pstmt.executeBatch();
     }
   }
+
+  static boolean isPgJsonColumn(String databaseName, String columnTypeName) {
+    return ("PostgreSQL".equalsIgnoreCase(databaseName)
+        && ("json".equalsIgnoreCase(columnTypeName) || "jsonb".equalsIgnoreCase(columnTypeName)));
+  }
+
 }
