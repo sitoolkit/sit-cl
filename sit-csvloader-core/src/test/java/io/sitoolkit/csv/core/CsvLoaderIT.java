d7f4191ae5971678fb7465076bbf5a95f3462633
haruki kasai <haruki.kasai@monocrea.co.jp>
2022-01-14 14:51:06 +0900

    Add multiple location test

--- sit-csvloader-core/src/test/java/io/sitoolkit/csv/core/CsvLoaderIT.java
+++ sit-csvloader-core/src/test/java/io/sitoolkit/csv/core/CsvLoaderIT.java
@@ -1,6 +1,8 @@
 package io.sitoolkit.csv.core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,35 +11,75 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.util.Properties;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CsvLoaderIT {
+  private Connection connection;
 
-  @Test
-  void loadTest() throws Exception {
-
+  @BeforeEach
+  void setup() throws Exception {
     Properties prop = new Properties();
     prop.load(getClass().getResourceAsStream("/connection.properties"));
 
-    Connection connection = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("user"),
+    connection = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("user"),
         prop.getProperty("password"));
 
     String createTable = Files.readString(Path.of(getClass().getResource("CREATE_TABLE.sql").toURI()));
-    String idenfifierQuateString = connection.getMetaData().getIdentifierQuoteString();
-    createTable = createTable.replace("\"", idenfifierQuateString);
+    String identifierQuoteString = connection.getMetaData().getIdentifierQuoteString();
+    createTable = createTable.replace("\"", identifierQuoteString);
     connection.createStatement().executeUpdate(createTable);
+  }
 
+  @Test
+  void loadTest() throws Exception {
     CsvLoader.load(connection, getClass(), (line) -> System.out.println(line));
 
-    String selectFronOrder = "SELECT * FROM \"ORDER\"".replace("\"", connection.getMetaData().getIdentifierQuoteString());
+    String selectFronOrder = "SELECT * FROM \"ORDER\"".replace("\"",
+        connection.getMetaData().getIdentifierQuoteString());
     ResultSet rs = connection.createStatement().executeQuery(selectFronOrder);
 
-    while (rs.next()) {
-      assertEquals(1, rs.getInt("FROM"));
-      assertEquals("one", rs.getString("COL_VARCHAR"));
-      assertEquals("2020-12-29", rs.getString("COL_DATE"));
-      assertEquals("12:30:00", rs.getString("COL_TIME"));
-      assertEquals(true, rs.getBoolean("COL_BOOLEAN"));
-    }
+    assertTrue(rs.next());
+
+    assertEquals(1, rs.getInt("FROM"));
+    assertEquals("one", rs.getString("COL_VARCHAR"));
+    assertEquals("2020-12-29", rs.getString("COL_DATE"));
+    assertEquals("12:30:00", rs.getString("COL_TIME"));
+    assertEquals(true, rs.getBoolean("COL_BOOLEAN"));
+
+    assertFalse(rs.next());
+  }
+
+  @Test
+  void loadMultipleLocationTest() throws Exception {
+    CsvLoader.load(connection, getClass(), (line) -> System.out.println(line), "one", "two");
+
+    String selectFromOrder = "SELECT * FROM \"ORDER\"".replace("\"",
+        connection.getMetaData().getIdentifierQuoteString());
+    ResultSet rs = connection.createStatement().executeQuery(selectFromOrder);
+
+    assertTrue(rs.next());
+
+    assertEquals(1, rs.getInt("FROM"));
+    assertEquals("one", rs.getString("COL_VARCHAR"));
+    assertEquals("2020-12-29", rs.getString("COL_DATE"));
+    assertEquals("12:30:00", rs.getString("COL_TIME"));
+    assertEquals(true, rs.getBoolean("COL_BOOLEAN"));
+
+    assertFalse(rs.next());
+
+    String selectFromOrder2 = "SELECT * FROM \"ORDER_2\"".replace("\"",
+        connection.getMetaData().getIdentifierQuoteString());
+    rs = connection.createStatement().executeQuery(selectFromOrder2);
+
+    assertTrue(rs.next());
+
+    assertEquals(2, rs.getInt("FROM"));
+    assertEquals("two", rs.getString("COL_VARCHAR"));
+    assertEquals("2022-01-14", rs.getString("COL_DATE"));
+    assertEquals("12:30:00", rs.getString("COL_TIME"));
+    assertEquals(false, rs.getBoolean("COL_BOOLEAN"));
+
+    assertFalse(rs.next());
   }
 }
