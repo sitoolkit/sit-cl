c9f47f92d836ee4e9b19375f050b64b5f5034688
suzuki-yuichi <yuichi.suzuki@monocrea.co.jp>
2023-11-20 09:35:40 +0900

    Mainクラスの変更、pom.xmlを修正

--- sit-csvloader-core/src/main/java/io/sitoolkit/csv/core/Main.java
+++ sit-csvloader-core/src/main/java/io/sitoolkit/csv/core/Main.java
@@ -1,50 +1,50 @@
 package io.sitoolkit.csv.core;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Properties;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
 
   public static void main(String[] args) {
-    new Main().execute();
+    new Main().execute(args);
   }
 
-  public void execute() {
-    Properties props = loadProperties();
-    try (Connection connection = createDatabaseConnection(props)) {
-      String dbType = getDatabaseType(props.getProperty("url"));
-      executeSqlScript(connection, dbType);
-      CsvLoader.load(connection, loadTableDataResources(), log -> System.out.println(log));
+  public void execute(String[] args) {
+    if (args.length < 2) {
+      return;
+    }
+
+    String connectionPropertiesPath = args[0];
+    String resourceDirectoryPath = args[1];
+
+    Properties connectionProps;
+    try {
+      connectionProps = loadProperties(connectionPropertiesPath);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return;
+    }
+
+    try (Connection connection = createDatabaseConnection(connectionProps)) {
+      processResources(connection, resourceDirectoryPath);
     } catch (SQLException | IOException e) {
       e.printStackTrace();
     }
   }
 
-  private Properties loadProperties() {
+  private Properties loadProperties(String filePath) throws IOException {
     Properties props = new Properties();
-    try (InputStream input =
-        getClass().getClassLoader().getResourceAsStream("connection.properties")) {
-      if (input == null) {
-        throw new IOException("Property file connection.properties is not found.");
-      }
+    try (FileInputStream input = new FileInputStream(filePath)) {
       props.load(input);
-    } catch (IOException e) {
-      e.printStackTrace();
     }
     return props;
   }
@@ -56,84 +56,41 @@ public class Main {
     return DriverManager.getConnection(url, user, password);
   }
 
-  private void executeSqlScript(Connection connection, String dbType)
+  private void processResources(Connection connection, String resourceDirectoryPath)
       throws IOException, SQLException {
-    String scriptToExecute = selectScriptForDatabase(dbType);
-    try (Statement statement = connection.createStatement()) {
-      statement.execute(scriptToExecute);
-    }
-  }
+    File dir = new File(resourceDirectoryPath);
+    File[] files = dir.listFiles();
 
-  private String getDatabaseType(String url) {
-    if (url.contains("mysql")) {
-      return "mysql";
-    } else if (url.contains("postgresql")) {
-      return "postgres";
-    } else {
-      return "h2";
+    if (files == null) {
+      return;
     }
-  }
 
-  private String selectScriptForDatabase(String dbType) throws IOException {
-    String sqlFileName = dbType.equals("mysql") ? "CREATE_TABLE_MySQL.sql" : "CREATE_TABLE.sql";
-    try (InputStream sqlScript = getClass().getClassLoader().getResourceAsStream(sqlFileName)) {
-      if (sqlScript == null) {
-        throw new IOException("SQL script file '" + sqlFileName + "' is not found.");
+    for (File file : files) {
+      if (file.getName().equals("table-list.txt")) {
+        processTableList(connection, file);
       }
-      return new String(sqlScript.readAllBytes(), StandardCharsets.UTF_8);
     }
   }
 
-  private List<TableDataResource> loadTableDataResources() throws IOException {
-    List<String> mainResourcePath = Arrays.asList("/");
-    List<String> testResourcePaths =
-        Arrays.asList(
-            "CsvLoaderIT/table-list.txt",
-            "one/CsvLoaderIT/table-list.txt",
-            "three/CsvLoaderIT/table-list.txt");
-
-    URL mainResourceUrl = getClass().getClassLoader().getResource("table-list.txt");
-    if (mainResourceUrl != null) {
-      return ResourceFinder.findTableDataResources(
-          getClass(), mainResourcePath, log -> System.out.println(log));
-    } else {
-      return testResourcePaths.stream()
-          .flatMap(
-              path -> {
-                try {
-                  return loadResourcesFromPath(path).stream();
-                } catch (IOException e) {
-                  e.printStackTrace();
-                  return Stream.empty();
-                }
-              })
-          .collect(Collectors.toList());
+  private void processTableList(Connection connection, File tableListFile)
+      throws IOException, SQLException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(tableListFile))) {
+      String tableName;
+      while ((tableName = reader.readLine()) != null) {
+        String csvFilePath = tableListFile.getParent() + "/" + tableName + ".csv";
+        File csvFile = new File(csvFilePath);
+        if (csvFile.exists()) {
+          processCsvFile(connection, csvFile, tableName);
+        }
+      }
     }
   }
 
-  private List<TableDataResource> loadResourcesFromPath(String path) throws IOException {
-    InputStream tableListStream = getClass().getClassLoader().getResourceAsStream(path);
-    if (tableListStream == null) {
-      return Collections.emptyList();
-    }
-
-    try (BufferedReader reader = new BufferedReader(new InputStreamReader(tableListStream))) {
-      return reader
-          .lines()
-          .map(tableName -> createTableDataResource(path, tableName))
-          .filter(Objects::nonNull)
-          .collect(Collectors.toList());
-    }
-  }
-
-  private TableDataResource createTableDataResource(String path, String tableName) {
-    String csvFilePath = path.substring(0, path.lastIndexOf('/')) + "/" + tableName + ".csv";
-    URL csvUrl = getClass().getClassLoader().getResource(csvFilePath);
-    if (csvUrl == null) {
-      return null;
-    }
-    String baseTableName =
-        tableName.contains("/") ? tableName.substring(tableName.lastIndexOf("/") + 1) : tableName;
-    return new TableDataResource(baseTableName, csvUrl);
+  private void processCsvFile(Connection connection, File csvFile, String tableName)
+      throws IOException, SQLException {
+    LogCallback log = System.out::println;
+    List<TableDataResource> resources = new ArrayList<>();
+    resources.add(new TableDataResource(tableName, csvFile.toURI().toURL()));
+    CsvLoader.load(connection, resources, log);
   }
 }
